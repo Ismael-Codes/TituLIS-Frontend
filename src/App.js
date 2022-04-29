8dcc3df3020a72f5631fab071422a13c444d41ba
Ismael Dominguez <ismael@uaeh.edu.mx>
2022-04-29 11:03:25 -0500

    new change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my first node page <code>src/App.js</code> and save to reload.
+          Esto es lo nuevo <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
