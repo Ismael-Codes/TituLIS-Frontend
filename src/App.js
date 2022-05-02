f7ef84012fa022952bce2679208068b2f3173879
Brian Cruz Sanchez <briancs1999@outlook.com>
2022-05-02 16:24:34 -0500

    cambie el nombre

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           Learn React
 
         </a>
-        <h1>Dr. María Nereida </h1>
+        <h1>Dr. María Nereida, Doctor Profesor Patricio </h1>
       </header>
     </div>
   );
