949db74bfdc2589c55e0c002d44c4ecb0dd9fb14
Ismael Dominguez <ismael@uaeh.edu.mx>
2022-05-10 14:35:23 -0500

    new button

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div>
-            <h1>Dashboarasdasdsad page</h1>
+            <h1>Dashboard page</h1>
             <input type="button" value = "pushmeee"/>
         </div>
     );
