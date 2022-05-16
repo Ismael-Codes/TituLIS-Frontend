1079b353148c183bb185210be8b2209248996bff
emmanuel <cr41555@uaeh.edu.mx>
2022-05-16 17:16:07 -0500

    se realizo el login para acceder al sistema

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Dashboard = () => {
     return (
-        <div>
-            <h1>Dashboard page</h1>
+        <div style={{background:"blue",margin:"0"}}>
+            <header style={{width:"100vw",background:"red",padding:""}}>
+                <img alt={} src={null}></img>
+                <h1>Titulacion ingenieria de software</h1>
+            </header>
+            <main>
+                <h2>Acceso</h2>
+                <input type="text" placeholder="No. cuenta"></input>
+                <input type="password" placeholder="Nip"></input>
+                <button>Iniciar session</button>
+
+
+            </main>
         </div>
     );
 };
