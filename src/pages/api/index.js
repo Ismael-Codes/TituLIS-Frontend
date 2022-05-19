80fa59698e997f1ddd3fad1afcf5906cec0ea3bb
Ismael Dominguez <ismael@uaeh.edu.mx>
2022-05-19 13:07:28 -0500

    checking node

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,16 +1,22 @@
-const app = require('express')();
+const express = require('express')();
+const app = express();
 const { v4 } = require('uuid');
+const port = process.env.PORT||4000;
 
-app.get('/api', (req, res) => {
-    const path = `/api/item/${v4()}`;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+app.listen(port)
+console.log('Listen on port ${port}')
 
-app.get('/api/item/:slug', (req, res) => {
-    const { slug } = req.params;
-    res.end(`Item: ${slug}`);
-});
-
-module.exports = app;
+//
+// app.get('/api', (req, res) => {
+//     const path = `/api/item/${v4()}`;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+//     res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+// });
+//
+// app.get('/api/item/:slug', (req, res) => {
+//     const { slug } = req.params;
+//     res.end(`Item: ${slug}`);
+// });
+//
+// module.exports = app;
