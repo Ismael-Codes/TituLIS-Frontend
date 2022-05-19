70a8666fcb0db68d6225cc8933b510c3acd0443c
g4m4l01d <es414610@uaeh.edu.mx>
2022-05-19 17:52:05 -0500

    Agregue una bargraph

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,11 +1,81 @@
 import React from 'react';
 
-const Statistics = () => {
-    return (
-        <div>
-            <h1>Statistics page</h1>
+
+
+class Statistics extends React.Component{
+
+    renderLines() {
+        return Array(10).fill(null).map((el,i)=>(
+            <BarGraphLine
+                Left={i*10}
+                key={i}
+            />
+        ))
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Estadisticas</h1>
+
+                <div className={"barGraph"}>
+                    <h3>Estatus</h3>
+                    <div className={"graph"}>
+                        <BarTextContent />
+                        <div className={"bar-lines-container"}>
+                            <RBar percent={50}/>
+                            <PBar percent={30}/>
+                            <CBar percent={20}/>
+                            {this.renderLines()}
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        );
+    }
+
+}
+
+const BarGraphLine = ({Left}) => {
+    return(
+        <div className="BarGraphLine"
+           style={{left: `${Left}%`}}
+        />
+    )
+
+}
+
+const BarTextContent = () => {
+    return(
+        <div className={"bar-text-content"}>
+            <text className={"chartlabel"}>Revision</text>
+            <div className={"labeldiv"}/>
+            <text className={"chartlabel"}>En Progreso</text>
+            <div className={"labeldiv"}/>
+            <text className={"chartlabel"}>Cancelada</text>
         </div>
-    );
-};
+    )
+
+}
+
+const RBar = ({percent}) => {
+  return(
+      <div className={"rbar"} style={{width: `${percent}%`}}></div>
+  )
+}
+
+const PBar = ({percent}) => {
+    return(
+        <div className={"pbar"} style={{width: `${percent}%`}}></div>
+    )
+}
+
+const CBar = ({percent}) => {
+    return(
+        <div className={"cbar"} style={{width: `${percent}%`}}></div>
+    )
+}
 
 export default Statistics;
