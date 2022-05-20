a674c081c3736d0cfed5183452aee2bdfbcdf358
Ismael-Codes <56968737+Ismael-Codes@users.noreply.github.com>
2022-05-20 15:45:49 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+
 import Sidebar from './components/Sidebar';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Dashboard from "./pages/Dashboard.jsx";
