04dcba7683e2bb24fcfc4b4d1dc81203eb628602
Ismael Dominguez <ismael@uaeh.edu.mx>
2022-05-30 15:16:13 -0500

    demo

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
               <Route path="/dashboard" element={<Dashboard/>}/>
               <Route path="/notifications" element={<Notifications/>}/>
               <Route path="/revision" element={<Revision/>}/>
-              <Route path="/statistics" element={<Statistics/>}/>
+              <Route path="/statisticsfor demo" element={<Statistics/>}/>
               <Route path="/LogOut" element={<LogOut/>} className="LogOut" />
             </Routes>
           </Sidebar>
