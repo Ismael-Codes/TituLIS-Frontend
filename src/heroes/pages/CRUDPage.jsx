8eb8bdf17b23f1d1227ef35aa6f2dc8f46b2c6b1
Brian Cruz Sanchez <BrianCS1999@outlook.com>
2022-08-06 19:38:34 -0500

    test

--- src/heroes/pages/CRUDPage.jsx
+++ src/heroes/pages/CRUDPage.jsx
@@ -1,11 +1,9 @@
-import { HeroList } from "../components"
 
 export const CRUDPage = () => {
   return (
     <>
       <h1>CRUD</h1>
       <hr />
-      {/* <HeroList publisher={'DC Comics'}/> */}
     </>
   )
 }
