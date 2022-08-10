6de185f4957cf8de8ca3ed0a570dfcefceebccc4
Brian Cruz Sanchez <BrianCS1999@outlook.com>
2022-08-10 14:33:43 -0500

    validation with the database v0.3

--- src/auth/helpers/validarUsuario.js
+++ src/auth/helpers/validarUsuario.js
@@ -1,11 +1,7 @@
 export const validarUsuario = (valid = false, email = '', data = '', userType = 2) => {
 
   const { message = ' ' } = data;
-
-  /* for (let i = 0; i < message.length; i++) {
-    (message[i].email === email) ? valid = true : valid;
-  } */
-
+  
   for (let i = 0; i < message.length; i++) {
     if (message[i].email === email) {
 
--- src/auth/hooks/useSignIn.js
+++ src/auth/hooks/useSignIn.js
@@ -1,8 +1,9 @@
 //? Primeros
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 
 //? Terceros
 import jwt_decode from 'jwt-decode';
+import axios from 'axios';
 
 //?
 import { useNavigate } from 'react-router-dom';
@@ -16,25 +17,22 @@ export const useSignIn = () => {
   const { login } = useContext(AuthContext)
   const navigate = useNavigate();
 
+  const url = 'https://express-with-vercel-iota.vercel.app'
+
   // const { data, isLoading, hasError } = useFetch('https://restserver-node-brian.herokuapp.com/api/usuarios/?limite=15&desde=0')
+
+  //* Despierta la base de datos
+  // useFetch(`${url}/api/saludar`)
+  useFetch('https://express-with-vercel-iota.vercel.app/api/saludar')
+
+  // const { data, isLoading, hasError } = useFetch(`${url}/api/getUsers`)
   const { data, isLoading, hasError } = useFetch('https://express-with-vercel-iota.vercel.app/api/getUsers')
 
-  
   const SignIn = (response) => {
-    console.log(data)
 
     const { email, given_name, family_name, picture, sub } = jwt_decode(response.credential);
 
     const { valid, userType } = validarUsuario(false, email, data);
-
-    if (valid) {
-
-      console.log('El usuario ya esta registrado')
-    } else {
-
-      //todo: hacer post a la base de datos
-      console.log('Usuario no registrado')
-    }
 
     //* Divide los nombres
     const miCadena = family_name;
@@ -45,6 +43,51 @@ export const useSignIn = () => {
     //* Extraer la matricula de un email
     const stringWithNumbers = email;
     const matricula = stringWithNumbers.replace(/[^0-9]+/g, "");
+
+    if (valid) {
+
+      console.log('El usuario ya esta registrado')
+    } else {
+
+
+      const dataTest = {
+        nombre: 'Brian',
+        aPaterno,
+        aMaterno,
+        matricula,
+        email
+      }
+
+
+      axios.post('http://897f-189-201-149-240.ngrok.io/api/saveUser', dataTest)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+
+      // fetch(`${url}/api/saveUser`, {
+     /*  fetch(`http://897f-189-201-149-240.ngrok.io/api/saveUser`, {
+        method: 'POST',
+        body: JSON.stringify({
+          'nombre': 'Brian',
+          aPaterno,
+          aMaterno,
+          matricula,
+          email
+        }),
+        headers: {
+          'Content-type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+        //todo: hacer post a la base de datos
+        */
+        console.log('El usuario ha sido registrado')
+    }
 
     login(given_name, aPaterno, aMaterno, email, matricula, picture, sub, userType);
     // login(name);
