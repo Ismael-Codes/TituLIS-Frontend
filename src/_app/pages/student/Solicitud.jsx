1995be34e6056a5baab0a693d9fd17ee55bbd8c5
Brian Cruz Sanchez <BrianCS1999@outlook.com>
2022-08-14 13:06:53 -0500

    Solicitud Page v0.3

--- src/_app/pages/student/Solicitud.jsx
+++ src/_app/pages/student/Solicitud.jsx
@@ -5,81 +5,86 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
 //? Select
-const currencies = [
+const forms = [
   {
-    value: 'USD',
-    label: '$',
+    value: '1',
+    label: 'Elaboración Tesis',
   },
   {
-    value: 'EUR',
-    label: '€',
+    value: '2',
+    label: 'Examen Teórico - Práctico',
   },
   {
-    value: 'BTC',
-    label: '฿',
+    value: '3',
+    label: 'Alto Rendimiento Académico',
   },
   {
-    value: 'JPY',
-    label: '¥',
+    value: '4',
+    label: 'Realización de estudios de Maestría',
   },
+  {
+    value: '5',
+    label: 'Realización de estudios de Doctorado',
+  },
+  {
+    value: '6',
+    label: 'Titulación como primer autor de artículo científico<',
+  }
 ];
 
 export const Solicitud = () => {
 
-
   const { formState, getValues, watch, register, handleSubmit } = useForm();
   const { errors } = formState;
 
+  //^ No es necesario
   watch();
 
   const onSubmit = (data) => alert(JSON.stringify(data, null, 4));
-
-
-
-  // const { register, handleSubmit, control } = useForm();
-  // const onSubmit = (data) => console.log(data);
 
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Grid container spacing={2} className="mb-3">
+          <Grid item='true' xs={12} textAlign="center">
+            <h1>Proceso de Solicitud</h1>
+          </Grid>
           <Grid item='true' xs={12} md={6}>
             <TextField
               sx={{ width: 1 }}
-              label="Nombre"
-              {...register("firstName")}
-              inputProps={register('firstName', {
-                required: 'Please enter First Name',
+              label="Ejemplo"
+              {...register("ejemplo")}
+              inputProps={register('ejemplo', {
+                required: 'Por favor ingrese un ejemplo',
               })}
-              error={errors.firstName}
-              helperText={errors.firstName?.message}
+              error={errors.ejemplo}
+              helperText={errors.ejemplo?.message}
             />
           </Grid>
           <Grid item='true' xs={12} md={6}>
             <TextField
               sx={{ width: 1 }}
-              label="Correo"
-              {...register("email")}
-              inputProps={register('email', {
-                required: 'Please enter email',
+              label="Ejemplo 2"
+              {...register("ejemplo2")}
+              inputProps={register('ejemplo2', {
+                required: 'Por favor ingrese un ejemplo 2',
               })}
-              error={errors.email}
-              helperText={errors.email?.message}
+              error={errors.ejemplo2}
+              helperText={errors.ejemplo2?.message}
             />
           </Grid>
           <Grid item='true' xs={12} md={4}>
             <TextField
               select
               fullWidth
-              defaultValue=""
-              label="Select"
-              inputProps={register('currency', {
-                required: 'Please enter currency',
+              label="Modalidad de Titulación"
+              inputProps={register('form', {
+                required: 'Por favor seleccione una modalidad',
               })}
-              error={errors.currency}
-              helperText={errors.currency?.message}
+              error={errors.form}
+              helperText={errors.form?.message}
             >
-              {currencies.map((option) => (
+              {forms.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
                   {option.label}
                 </MenuItem>
@@ -88,10 +93,9 @@ export const Solicitud = () => {
           </Grid>
           <Grid item='true' xs={12}>
             <Button type="submit" variant="contained" color="success" size="large">
-              submit
+              enviar Solicitud
             </Button>
           </Grid>
-
           <Grid item='true' xs={12}>
             <pre>{JSON.stringify(getValues(), null, 4)}</pre>
           </Grid>
