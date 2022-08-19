7ce099bb571dd8faa7b5bc5baf6dcf721057df81
Brian Cruz Sanchez <BrianCS1999@outlook.com>
2022-08-19 17:51:58 -0500

    Subir Archivos a Google Drive

--- src/_app/pages/student/Estado.jsx
+++ src/_app/pages/student/Estado.jsx
@@ -1,60 +1,30 @@
-import { Card } from "@mui/material"
-import Box from '@mui/material/Box';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { Fragment } from "react";
-import Grid from '@mui/system/Unstable_Grid';
 
 export const Estado = () => {
 
-  const bull = (
-    <Box
-      component="span"
-      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-    >
-      •
-    </Box>
-  );
+  function guardarArchivo(e) {
+    var file = e.target.files[0] //the file
+    var reader = new FileReader() //this for convert to Base64 
+    reader.readAsDataURL(e.target.files[0]) //start conversion...
+    reader.onload = function (e) { //.. once finished..
+      var rawLog = reader.result.split(',')[1]; //extract only thee file data part
+      var dataSend = { dataReq: { data: rawLog, name: file.name, type: file.type }, fname: "uploadFilesToGoogleDrive" }; //preapre info to send to API
+      fetch('https://script.google.com/macros/s/AKfycbxKgctsO8FsgaW8L4QGQG8WPlIY2c3a4j426oQ9wquzRSA4pcZBC3-sdnY96k244qSJ/exec', //your AppsScript URL
+        { method: "POST", body: JSON.stringify(dataSend) }) //send to Api
+        .then(res => res.json()).then((a) => {
+          console.log(a, 'Exito') //See response
+        }).catch(e => console.log(e, 'Error')) // Or Error in console
+    }
+  }
 
-  const card = (
-    <Fragment>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Modalidad
-        </Typography>
-        <Typography variant="h5" component="div">
-          Titulo
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button variant="contained" color="error" size="small">Cancelar</Button>
-        <Button variant="contained" size="small">Editar</Button>
-      </CardActions>
-    </Fragment>
-  );
 
   return (
     <>
-      <Grid container spacing={2} className="mb-3">
-        <Grid item='true' xs={12} textAlign="center">
-          <h1>Estado de Solicitud</h1>
-        </Grid>
-        <Grid item='true' xs={12} md={6}>
-          <Box sx={{ minWidth: 275 }} className="col-6">
-            <Card variant="outlined">{card}</Card>
-          </Box>
-        </Grid>
-      </Grid>
+      <h1>Subir Archivo A Google</h1>
+      <hr />
+      {/* <input type="file" id="customFile" onChange={(e) => guardarArchivo(e)} /> */}
+      <input type="file" accept="application/pdf" id="customFile" onChange={(e) => guardarArchivo(e)} />
     </>
   )
 }
+
+
