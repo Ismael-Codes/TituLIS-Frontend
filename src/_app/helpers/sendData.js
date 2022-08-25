176346dc2c9c823197f6bd80663af92c64a1bd99
Brian Cruz Sanchez <BrianCS1999@outlook.com>
2022-08-24 22:17:37 -0500

    solicitud process done

--- src/_app/helpers/sendData.js
+++ src/_app/helpers/sendData.js
@@ -3,16 +3,21 @@ export const sendData = (inputFileData, inputTextData, inputSelectData, dataForm
 
   let solicitud = [];
   let files = [];
+  let filesHelper = [];
   let inputText = [];
+  let inputHelper = [];
   let inputSelect = [];
+  let selectHelper = [];
 
   //* Arvhicos que se enviaran
   if (inputFileData != undefined) {
     for (let i = 0; i < inputFileData.length; i++) {
       const code = inputFileData[i].code;
       if (dataForm[code] != undefined) {
+        const helper = inputFileData[i].name
         const data = dataForm[code].file;
-        files = [...files, { code, file: data }]
+        files = [...files, { code, data }]
+        filesHelper = [...filesHelper, helper]
       }
     }
   }
@@ -22,8 +27,10 @@ export const sendData = (inputFileData, inputTextData, inputSelectData, dataForm
     for (let i = 0; i < inputTextData.length; i++) {
       const code = inputTextData[i].code;
       if (dataForm[code] != undefined) {
+        const helper = inputTextData[i].name
         const data = dataForm[code];
         inputText = [...inputText, { code, data }]
+        inputHelper = [...inputHelper, { helper, data }]
       }
     }
   }
@@ -33,13 +40,14 @@ export const sendData = (inputFileData, inputTextData, inputSelectData, dataForm
     for (let i = 0; i < inputSelectData.length; i++) {
       const code = inputSelectData[i].code;
       if (dataForm[code] != undefined) {
+        const helper = inputSelectData[i].name
         const data = dataForm[code];
         inputSelect = [...inputSelect, { code, data }]
+        selectHelper = [...selectHelper, { helper, data }]
       }
     }
   }
 
-  solicitud = [inputSelect, inputText, files]
-  console.log(solicitud, 'Solicitud')
-
+  solicitud = [inputSelect, inputText, files, dataForm['form'], filesHelper, inputHelper, selectHelper]
+  return solicitud;
 }
